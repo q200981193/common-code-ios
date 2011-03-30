07cbbf444f08c6a9d4a1dd52b2744a6f9c5b110a
Caleb Davenport <caleb@guicocoa.com>
2011-03-30 14:39:45 -0400

    enabled singleton of location manager

--- Categories/CLLocationManager+GCExtensions.h
+++ Categories/CLLocationManager+GCExtensions.h
@@ -6,16 +6,12 @@
 //  Copyright 2011 GUI Cocoa, LLC. All rights reserved.
 //
 
-#ifdef GC_CORE_LOCATION
-
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
 @interface CLLocationManager (GCExtensions)
 
-//+ (CLLocationManager *)gc_sharedManager;
++ (CLLocationManager *)gc_sharedManager;
 + (BOOL)gc_isLocationAvailable;
 
 @end
-
-#endif
--- Categories/CLLocationManager+GCExtensions.m
+++ Categories/CLLocationManager+GCExtensions.m
@@ -6,13 +6,11 @@
 //  Copyright 2011 GUI Cocoa, LLC. All rights reserved.
 //
 
-#ifdef GC_CORE_LOCATION
-
 #import "CLLocationManager+GCExtensions.h"
 
 @implementation CLLocationManager (GCExtensions)
 
-//GC_SINGLETON_INSTANCE(CLLocationManager, gc_sharedManager);
+GC_SINGLETON_INSTANCE(CLLocationManager, gc_sharedManager);
 + (BOOL)gc_isLocationAvailable {
     BOOL enabled = [CLLocationManager locationServicesEnabled];
 	if (NSFoundationVersionNumber >= NSFoundationVersionNumber_iOS_4_2) {
@@ -27,5 +25,3 @@
 }
 
 @end
-
-#endif

6be7074caf8a296cb185e71fa43d436ff4e68ece
Caleb Davenport <caleb@guicocoa.com>
2011-04-18 13:26:02 -0400

    added iphone specific browser view controller

---
+++ Photo Picker/View Controllers/GCImageBrowserViewController_iPhone.h
@@ -0,0 +1,15 @@
+//
+//  GCImageBrowserViewController_iPhone.h
+//  GUI Cocoa Common Code Library for iOS
+//
+//  Created by Caleb Davenport on 4/18/11.
+//  Copyright 2011 GUI Cocoa, LLC. All rights reserved.
+//
+
+#import "GCImageBrowserViewController.h"
+
+@interface GCImageBrowserViewController_iPhone : GCImageBrowserViewController {
+    
+}
+
+@end
---
+++ Photo Picker/View Controllers/GCImageBrowserViewController_iPhone.m
@@ -0,0 +1,38 @@
+//
+//  GCImageBrowserViewController_iPhone.m
+//  GUI Cocoa Common Code Library for iOS
+//
+//  Created by Caleb Davenport on 4/18/11.
+//  Copyright 2011 GUI Cocoa, LLC. All rights reserved.
+//
+
+#import "GCImageBrowserViewController_iPhone.h"
+
+@implementation GCImageBrowserViewController_iPhone
+
+#pragma mark - object lifecycle
+- (id)initWithBrowser:(GCImageBrowserController *)browser {
+    self = [super initWithBrowser:browser];
+    if (self) {
+        self.wantsFullScreenLayout = YES;
+    }
+    return self;
+}
+
+#pragma mark - view lifecycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    CGFloat top = self.navigationController.navigationBar.frame.size.height;
+    top += [[UIApplication sharedApplication] statusBarFrame].size.height;
+    self.browser.tableView.contentInset = UIEdgeInsetsMake(top + self.browser.tableView.contentInset.top, 0, 0, 0);
+    self.browser.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(top, 0, 0, 0);
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackTranslucent animated:animated];
+}
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
